ada1cd3de4efbfd3fe7dee07137d7f18b738240b
Miles Angels <milesmarieangels@gmail.com>
2022-05-08 11:53:47 -0700

    answer to exercise

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,12 @@
-let js = 'amazing';
-      if(js === 'amazing') {
-        alert('JavaScript is FUN!');
-      }
+//1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
 
-      console.log(40 + 8 + 23 - 10);
+//2. Log their values to the console
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = 329.5;
+
+console.log('country: ' + country);
+console.log('continent: ' + continent);
+console.log('population: ' + population + ' million');
